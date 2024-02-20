aecc6bcd7c0961f8a0fe9b9317ed478fa2811e51
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2024-02-20 21:41:50 +0000

    Deploying to gh-pages from @ Maxym87/goit-js-hw-09@caff9f32dcc80c703f3bba9c38a34fe805fa7e08 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const e=document.querySelector(".feedback-form");e.addEventListener("input",a=>{const t={email:e.email.value,message:e.message.value};localStorage.setItem("feedback-form-state",JSON.stringify(t))});
+//# sourceMappingURL=commonHelpers2.js.map
