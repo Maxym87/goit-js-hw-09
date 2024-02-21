f53291168a4be79a1bb3170e311d8fca72283c94
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2024-02-21 16:21:30 +0200

    add function remove local stor

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,4 @@
+const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', event => {
@@ -5,5 +6,28 @@ form.addEventListener('input', event => {
     email: form.email.value,
     message: form.message.value,
   };
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+const rawData = localStorage.getItem(STORAGE_KEY);
+
+if (rawData) {
+  const data = JSON.parse(rawData);
+  form.email.value = data.email;
+  form.message.value = data.message;
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  
+  localStorage.removeItem(STORAGE_KEY);
+
+  form.email.value = '';
+  form.message.value = '';
+
+  const formData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+  console.log(formData);
 });
